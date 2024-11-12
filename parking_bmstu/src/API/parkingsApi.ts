c5e4c53c88b667222c149edfa6ce264678a45d36
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2024-11-12 20:26:53 +0300

    Внедрил прокси

--- parking_bmstu/src/API/parkingsApi.ts
+++ parking_bmstu/src/API/parkingsApi.ts
@@ -1,27 +1,21 @@
 // src/API/parkingsApi.ts
-const BASE_URL = 'http://localhost:8000';
-
 export const fetchParkings = async () => {
-  const url = new URL(`${BASE_URL}/parkings/`);
-
-  const response = await fetch(url.toString());
+  const response = await fetch('/parkings');  // Здесь используем относительный путь
   if (!response.ok) {
     throw new Error('Ошибка загрузки парковок');
   }
 
   const data = await response.json();
-  return Array.isArray(data) ? data : data.parkings || []; // Проверяем, что данные — массив
+  return Array.isArray(data) ? data : data.parkings || [];
 };
 
-
 export const fetchParkingById = async (id: number) => {
-    const response = await fetch(`${BASE_URL}/parkings/${id}/`);
-    if (!response.ok) {
-      throw new Error('Ошибка загрузки данных парковки');
-    }
-  
-    const data = await response.json();
-    console.log("Детали парковки из API:", data); // Отладка
-    return data;
-  };
-  
+  const response = await fetch(`/parkings/${id}`);  // Используем относительный путь
+  if (!response.ok) {
+    throw new Error('Ошибка загрузки данных парковки');
+  }
+
+  const data = await response.json();
+  console.log('Детали парковки из API:', data); // Отладка
+  return data;
+};
