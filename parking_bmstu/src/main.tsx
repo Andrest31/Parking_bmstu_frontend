5f52cc9b0d1c2d243f68eb7bc80e3701fc203d61
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2024-11-20 02:21:52 +0300

    Внедрил PWA

--- parking_bmstu/src/main.tsx
+++ parking_bmstu/src/main.tsx
@@ -5,6 +5,23 @@ import store from './store/store'; // Импорт Redux Store
 import './index.css';
 import App from './App';
 
+// Условие для регистрации сервис-воркера только в продакшн-режиме
+if (process.env.NODE_ENV === 'production') {
+  // Проверка, поддерживает ли браузер сервис-воркеры
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')  // Путь к вашему сервис-воркеру
+        .then((registration) => {
+          console.log('Service Worker зарегистрирован с областью: ', registration.scope);
+        })
+        .catch((error) => {
+          console.log('Ошибка регистрации Service Worker: ', error);
+        });
+    });
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
