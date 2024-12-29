855684884449e443223e242270fb7d010442cf06
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2024-12-29 18:35:34 +0300

    header

--- parking_bmstu/src/components/Header/Header.tsx
+++ parking_bmstu/src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto nav-links">
-              <Nav.Link as={Link} to="/passes" className="nav-link">Заявки</Nav.Link>
+              <Nav.Link as={Link} to="/passes" className="nav-link">Абонементы</Nav.Link>
                 <Nav.Link as={Link} to="/" className="nav-link">Парковки</Nav.Link>
                 <Nav.Link as={Link} to="/login" className="nav-link">Войти</Nav.Link>
                 <Nav.Link as={Link} to="/profile" className="nav-link">Профиль</Nav.Link>
